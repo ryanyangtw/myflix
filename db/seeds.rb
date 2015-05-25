d71aec946e479d341a21ef6c8894ffa3dde69e3f
Ryan <hiphubryan@gmail.com>
2015-05-25 17:39:24 +0800

    Finish HW4 & Add video mosel & Add video seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+Video.create(title: "Futurama", description: "A show about the future!", small_cover_url: '/public/tmp/futurama.jpg', large_cover_url: '/tmp/futurama.jpg' )
+Video.create(title: "Monk", description: "Not sure what this show is about.", small_cover_url: '/public/tmp/monk.jpg', large_cover_url: '/tmp/monk_large.jpg')
+Video.create(title: "Family Guy", description: "Family Guy is an American adult animated sitcom created by Seth MacFarlane", small_cover_url: '/tmp/family_guy.jpg', large_cover_url: '/tmp/family_guy.jpg')
