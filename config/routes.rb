b5f06f646c15818b85fd290d9efdd33255c34d62
Ryan <hiphubryan@gmail.com>
2015-05-25 18:29:27 +0800

    Finish Precourse

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Myflix::Application.routes.draw do
+  
+  get 'home', to: "videos#index"
+  resources :videos, only: [:index, :show]
+  resources :categories, only: :show
+  
+
   get 'ui(/:action)', controller: 'ui'
+
 end
