46961d4842d28d4f56f41a521335afd6fe6dcb0b
Ryan <hiphubryan@gmail.com>
2015-05-26 17:26:16 +0800

    Finish Assignment: Add authentication

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,5 +1,7 @@
 class VideosController < ApplicationController
 
+  before_action :require_user
+
   def index
     @categories = Category.all
   end
--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,10 @@ Myflix::Application.routes.draw do
 
   get 'register', to: "users#new"
   get 'sign_in', to: "sessions#new"
+  delete 'sign_out', to: "sessions#destroy"
+
   resources :users, only: :create 
+  resources :sessions, only: :create
   
 
   get 'ui(/:action)', controller: 'ui'
