608c2140c881ca1d8678f07cae623a82350854c0
Ryan <hiphubryan@gmail.com>
2015-06-01 00:28:23 +0800

    Finish Assignment: Reset Password

---
+++ app/controllers/password_resets_controller.rb
@@ -0,0 +1,28 @@
+class PasswordResetsController < ApplicationController
+  def show
+    user = User.find_by(token: params[:id])
+    if user
+      @token = user.token
+    else
+      redirect_to expired_token_path
+    end
+    
+  end
+
+  def create
+    user = User.find_by(token: params[:token])
+    if user
+      #user.update(password: params[:password])
+      user.password = params[:password]
+      user.generate_token
+      user.save
+      flash[:success] = "Your password has been changed. Please sign in."
+      redirect_to sign_in_path
+    else
+      redirect_to expired_token_path
+    end
+  end
+
+  def expired_token
+  end
+end
