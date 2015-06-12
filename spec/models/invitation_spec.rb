f799f09d2a18366eca51e9622fce41d239961404
Ryan <hiphubryan@gmail.com>
2015-06-12 20:28:24 +0800

    Rename Ivitation.rb to invitation.rb

--- spec/models/invitation_spec.rb
+++ spec/models/invitation_spec.rb
@@ -1,15 +1,15 @@
-# require 'rails_helper'
+require 'rails_helper'
 
 
 
-# RSpec.describe Invitation, :type => :model do
-#   it { should validate_presence_of(:recipient_name) }
-#   it { should validate_presence_of(:recipient_email) }
-#   it { should validate_presence_of(:message) }
-#   it { is_expected.to belong_to(:inviter) } 
+RSpec.describe Invitation, :type => :model do
+  it { should validate_presence_of(:recipient_name) }
+  it { should validate_presence_of(:recipient_email) }
+  it { should validate_presence_of(:message) }
+  it { is_expected.to belong_to(:inviter) } 
 
-#   it_behaves_like "tokenable" do
-#     let(:object) { Fabricate(:invitation) }
-#   end
+  it_behaves_like "tokenable" do
+    let(:object) { Fabricate(:invitation) }
+  end
   
-# end
+end
