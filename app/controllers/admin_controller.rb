0feafb66feed194d0601f06aba99790ba66079d6
Ryan <hiphubryan@gmail.com>
2015-06-20 11:09:22 +0800

    Refactor code by Tomas's suggestions

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,4 +1,14 @@
 class AdminController < ApplicationController
   before_action :require_user
   before_action :require_admin
+
+
+
+  def require_admin
+    if !current_user.admin?
+      flash[:error] = "You are not authorized to do that."
+      redirect_to home_path
+    end
+  end
+  
 end
